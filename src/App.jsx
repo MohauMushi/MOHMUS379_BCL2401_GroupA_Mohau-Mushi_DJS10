93e0ec47686856bedbbe3f036ba06c981ec0f38a
Mohau Mushi <david1mushi1@gmail.com>
2024-06-08 21:11:44 +0200

    Rendering the posts or error message from the api in the fetchPosts file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import React  from 'react'
-import { useFetchPosts } from './utils/fetchPosts'
-
+import { useFetchPosts } from "./utils/fetchPosts";
 
 function App() {
   const { posts, error } = useFetchPosts();
 
   return (
     <>
-
+      {error ? (
+        <div>Data fetching failed</div>
+      ) : (
+        <div>
+          <h1>Posts</h1>
+          {posts.map((post, index) => (
+            <div key={index}>
+              <h2>
+                {index + 1}. {post.title}
+              </h2>
+              <p>{post.body}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
